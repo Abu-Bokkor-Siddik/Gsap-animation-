c3b8dc4c2961a6847e0f059dc4b06f78345e25b2
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-01-31 10:17:23 +0600

    first commit

---
+++ gsap.js
@@ -0,0 +1,27 @@
+
+let posX = 0,
+  posY = 0;
+
+let mouseX = 0,
+  mouseY = 0;
+
+gsap.to(".cursor-example", {
+  duration: 0.018,
+  repeat: -1,
+  onRepeat: function () {
+    posX += (mouseX - posX) / 8;
+    posY += (mouseY - posY) / 8;
+
+    gsap.set(".cursor-example", {
+      css: {
+        left: posX - 1,
+        top: posY - 2
+      }
+    });
+  }
+});
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
