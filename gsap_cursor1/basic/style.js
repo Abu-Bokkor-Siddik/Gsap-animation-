496783afc21329bb8fb3c671a3a902488709ff8b
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-05 10:35:46 +0600

    marquee text animation logo

---
+++ gsap_cursor1/basic/style.js
@@ -0,0 +1,15 @@
+gsap.to('h1',{
+    x:500,
+    duration:2,
+   backgroundColor:'red',
+    ease:'bounce',
+    delay:1,
+   
+})
+gsap.from('.name',{
+    y:500,
+    duration:2,
+    delay:1
+   
+
+})
